a9beffca1c174749a3ea29f627062823e3d09704
kachkacha2003 <lasha.katchkatchishvili.1@btu.edu.ge>
2026-04-23 20:50:18 +0400

    added gitignore file

--- obj/Debug/net8.0/WebApplication8.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApplication8.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8fb53a6fcb8ae0fbbcd362b1f84bf9f712605e58")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
